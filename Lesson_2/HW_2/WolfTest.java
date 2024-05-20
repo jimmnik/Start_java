3a9072f0b500209553bafd7135638d5085b2b2ba
Никита Грошев <jimmnik3@gmail.com>
2024-05-20 13:55:00 +0300

    HW 2

---
+++ Lesson_2/HW_2/WolfTest.java
@@ -0,0 +1,21 @@
+public class WolfTest{
+
+	public static void main(String[] args) {
+		Wolf first = new Wolf();
+		first.go(7);
+		first.set_doun();
+		first.sprint(70);
+		System.out.println("Вес в начале " + first.getWeigt());
+		first.setWeigt(40);
+		System.out.println("Вес в конце " + first.getWeigt());
+		System.out.println("Имя пса " + first.getName());
+
+		Wolf second = new Wolf();
+		System.out.println("Вес в начале " + second.getWeigt());
+		second.setWeigt(10);
+		System.out.println("Вес в конце " + second.getWeigt());
+
+		System.out.println("Вес первого пса " + first.getWeigt());
+		System.out.println("Вес второго пса " + second.getWeigt());
+	}
+}
